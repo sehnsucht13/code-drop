288478cea8550a625ababa567b4309dc62f934e3
Yavor Konstantinov <ykonstantinov1@gmail.com>
2021-01-18 19:22:28 -0800

    Add "react-icons" package. Add "chevron" to each code line.

--- frontend/src/components/CodeLine.js
+++ frontend/src/components/CodeLine.js
@@ -1,8 +1,13 @@
 import React from "react";
+import { IconContext } from "react-icons";
+import { BsFillCaretRightFill } from "react-icons/bs";
 
 export default function CodeLine({ line, lineNum, lineProps, tokenProps }) {
   return (
     <div {...lineProps({ line, key: lineNum })}>
+      <IconContext.Provider value={{ color: "pink", size: "2%" }}>
+        <BsFillCaretRightFill />
+      </IconContext.Provider>
       <span style={{ padding: "2%" }}>{lineNum + 1}</span>
       {line.map((token, key) => (
         <span {...tokenProps({ token, key })} />
