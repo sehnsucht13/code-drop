d7629f7a3d08631cc5a293e8908254b6578c6cc7
Yavor Konstantinov <ykonstantinov1@gmail.com>
2021-01-25 17:40:09 -0800

    Implement route for creating new drops.

--- server/index.js
+++ server/index.js
@@ -1,19 +1,56 @@
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
-require("dotenv").config();
+const db = require("./models");
 
 let app = express();
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
-
 // parse application/json
 app.use(bodyParser.json());
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
   res.send("Hello World!");
 });
 
-app.listen(process.env.HOST_PORT, function () {
-  console.log(`Listening on port: ${process.env.HOST_PORT}`);
+app.post("/drops", (req, res) => {
+  let title = req.body.title;
+  let lang = req.body.lang;
+  let visibility = req.body.visibility;
+  let textBody = req.body.text;
+
+  db.Drops.create({
+    dropTitle: title,
+    dropLanguage: lang,
+    visibility: visibility,
+    dropText: textBody,
+  })
+    .then((thing) => {
+      console.log("thing", thing.dataValues.id);
+      res.send("Created successfully").status(200);
+    })
+    .catch(() => {
+      res.send("There was an error creating a new drop").status(400);
+    });
+  console.log("body", req.body);
+  console.log("Received", title, lang, visibility, textBody);
 });
+app.delete("/drops/:dropId", (req, res) => {
+  let dropId = req.params.dropId;
+});
+app.put("/drops/:dropId", (req, res) => {
+  let dropId = req.params.dropId;
+});
+
+db.sequelize
+  .sync()
+  .then(() => {
+    console.log("Connected to db");
+    app.listen(process.env.HOST_PORT, function () {
+      console.log(`Listening on port: ${process.env.HOST_PORT}`);
+    });
+  })
+  .catch(() => {
+    console.log("There is an issue with the db conection");
+  });
