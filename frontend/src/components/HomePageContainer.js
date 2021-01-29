0c930407587297383608febb54e7f05dbbd300a1
Yavor Konstantinov <ykonstantinov1@gmail.com>
2021-01-28 22:41:08 -0800

    Implement basic Home Page.

---
+++ frontend/src/components/HomePageContainer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBar from "./Navbar";
+
+function HomePageContainer() {
+  return (
+    <div>
+      <NavBar />
+      <p>Welcome to the home page!</p>
+    </div>
+  );
+}
+
+export default HomePageContainer;
