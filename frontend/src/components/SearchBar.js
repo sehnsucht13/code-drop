dc8c3d206fe679707120e165d8b45b5ecde41038
Yavor Konstantinov <ykonstantinov1@gmail.com>
2021-01-31 13:14:29 -0800

    Extract search bar into new component.

---
+++ frontend/src/components/SearchBar.js
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import { Button, Form, Col } from "react-bootstrap";
+import CodeMirrorLanguages from "../helpers/CodeMirrorLanguages";
+import { Link } from "react-router-dom";
+
+function SearchBar() {
+  const [titleInput, setTitleInput] = useState("");
+  const [timePeriod, setTimePeriod] = useState("all_time");
+  const [language, setLanguage] = useState("None");
+
+  const handleTitleInput = (ev) => {
+    setTitleInput(ev.target.value);
+  };
+
+  const handleLanguageSelect = (ev) => {
+    setLanguage(ev.target.value);
+  };
+
+  const handleTimeSelect = (ev) => {
+    setTimePeriod(ev.target.value);
+  };
+
+  return (
+    <Form>
+      <Form.Row>
+        <Form.Group as={Col} xs={12} sm={5}>
+          <Form.Label>Drop Title</Form.Label>
+          <Form.Control
+            onChange={handleTitleInput}
+            value={titleInput}
+            placeholder="Drop Title..."
+          ></Form.Control>
+        </Form.Group>
+        <Form.Group as={Col} xs={12} sm={3}>
+          <Form.Label> Language </Form.Label>
+          <Form.Control
+            as="select"
+            defaultValue="None"
+            onChange={handleLanguageSelect}
+          >
+            {Object.keys(CodeMirrorLanguages).map((key) => {
+              return <option value={key}>{key}</option>;
+            })}
+          </Form.Control>
+        </Form.Group>
+        <Form.Group as={Col} xs={12} sm={2}>
+          <Form.Label>Time Period</Form.Label>
+          <Form.Control
+            as="select"
+            defaultValue="all_time"
+            onChange={handleTimeSelect}
+          >
+            <option value="day">Day</option>
+            <option value="week">Week</option>
+            <option value="month">Month</option>
+            <option value="year">Year</option>
+            <option value="all_time">All Time</option>
+          </Form.Control>
+        </Form.Group>
+        <Form.Group as={Col} xs={12} sm={2}>
+          <Link
+            to={{
+              pathname: "/search",
+              search: `?contains=${titleInput}&l=${language}&t=${timePeriod}`,
+            }}
+          >
+            <Button>Search</Button>
+          </Link>
+        </Form.Group>
+      </Form.Row>
+    </Form>
+  );
+}
+
+export default SearchBar;
--- frontend/src/components/SearchContainer.js
+++ frontend/src/components/SearchContainer.js
@@ -1,4 +1,3 @@
-import "bootstrap/dist/css/bootstrap.min.css";
 import React, { useEffect, useState } from "react";
 import NavBar from "./Navbar";
 import { useLocation, Link } from "react-router-dom";
@@ -7,15 +6,12 @@ import queryString from "query-string";
 import CodeMirrorLanguages from "../helpers/CodeMirrorLanguages";
 import axios from "axios";
 
+import SearchBar from "./SearchBar";
+
 function SearchContainer() {
-  const [titleInput, setTitleInput] = useState("");
-  const [timePeriod, setTimePeriod] = useState("all_time");
-  const [language, setLanguage] = useState("None");
   // Stores the parsed query string parameters whenever the component is rendered again.
   let searchParams = queryString.parse(useLocation().search);
-
-  // TODO: Remove this log statement
-  // console.log("Params on route are", useLocation(), searchParams);
+  console.log("The page params are ", searchParams);
 
   // React.useEffect(() => {
   //   axios
@@ -25,71 +21,11 @@ function SearchContainer() {
   //     });
   // }, [searchParams]);
 
-  const handleTitleInput = (ev) => {
-    setTitleInput(ev.target.value);
-  };
-
-  const handleLanguageSelect = (ev) => {
-    setLanguage(ev.target.value);
-  };
-
-  const handleTimeSelect = (ev) => {
-    setTimePeriod(ev.target.value);
-    console.log(timePeriod, language, titleInput);
-  };
-
   return (
     <div>
       <NavBar />
       <Container fluid>
-        <Form>
-          <Form.Row>
-            <Form.Group as={Col} xs={12} sm={5}>
-              <Form.Label>Drop Title</Form.Label>
-              <Form.Control
-                onChange={handleTitleInput}
-                value={titleInput}
-                placeholder="Drop Title..."
-              ></Form.Control>
-            </Form.Group>
-            <Form.Group as={Col} xs={12} sm={3}>
-              <Form.Label> Language </Form.Label>
-              <Form.Control
-                as="select"
-                defaultValue="None"
-                onSelect={handleLanguageSelect}
-              >
-                {Object.keys(CodeMirrorLanguages).map((key) => {
-                  return <option value={key}>{key}</option>;
-                })}
-              </Form.Control>
-            </Form.Group>
-            <Form.Group as={Col} xs={12} sm={2}>
-              <Form.Label>Time Period</Form.Label>
-              <Form.Control
-                as="select"
-                defaultValue="all_time"
-                onChange={handleTimeSelect}
-              >
-                <option value="day">Day</option>
-                <option value="week">Week</option>
-                <option value="month">Month</option>
-                <option value="year">Year</option>
-                <option value="all_time">All Time</option>
-              </Form.Control>
-            </Form.Group>
-            <Form.Group as={Col} xs={12} sm={2}>
-              <Link
-                to={{
-                  pathname: "/search",
-                  search: `?contains=hello`,
-                }}
-              >
-                <Button>Search</Button>
-              </Link>
-            </Form.Group>
-          </Form.Row>
-        </Form>
+        <SearchBar />
       </Container>
     </div>
   );
