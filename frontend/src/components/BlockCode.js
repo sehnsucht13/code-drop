79f2774818b2e55188f0df086301edd36f8b44fd
Yavor Konstantinov <ykonstantinov1@gmail.com>
2021-01-30 17:37:09 -0800

    Refactor Block Code display into a display component.

--- frontend/src/components/BlockCode.js
+++ frontend/src/components/BlockCode.js
@@ -17,80 +17,65 @@ const generateMap = (annotations) => {
   return indexMap;
 };
 
-export default function BlockCodeView({ id }) {
-  // const lineNumPaddingLen = codeString.split("\n").length.toString().length;
+export default function BlockCodeView({
+  dropText,
+  dropLanguage,
+  dropTitle,
+  id,
+  visibility,
+  annotations,
+}) {
   const [paddingLength, setPaddingLength] = useState(0);
-  const [codeDrop, setCodeDrop] = useState(undefined);
-  const [annotations, setAnnotations] = useState(undefined);
-  const [isLoaded, setIsLoaded] = useState(false);
   const [indexMap, setIndexMap] = useState({});
 
   useEffect(() => {
-    axios
-      .get("/drops/21")
-      .then((response) => {
-        console.log(response.data);
-        setCodeDrop(response.data.codeDrop);
-        setAnnotations(response.data.dropAnnotations);
-        setIndexMap(generateMap(response.data.dropAnnotations));
-        setPaddingLength(
-          response.data.codeDrop.dropText.split("\n").length.toString().length
-        );
-        setIsLoaded(true);
-      })
-      .catch((err) => {
-        console.log("There was an error in retireveing data", err);
-      });
-  }, []);
+    setIndexMap(generateMap(annotations));
+    setPaddingLength(dropText.split("\n").length.toString().length);
+  }, [annotations, dropText]);
 
-  if (!isLoaded) {
-    return "Loading...";
-  } else {
-    return (
-      <>
-        <h1>{codeDrop.dropTitle}</h1>
-        <h3>{"Language: ".concat(codeDrop.dropLanguage)}</h3>
-        <Highlight
-          Prism={Prism}
-          theme={theme}
-          code={codeDrop.dropText}
-          language={codeDrop.dropLanguage}
-        >
-          {({ className, style, tokens, getLineProps, getTokenProps }) => (
-            <pre className={className} style={style}>
-              {tokens.map((line, i) => {
-                if (i in indexMap) {
-                  console.log(indexMap[i]);
-                  return (
-                    <CodeLine
-                      line={line}
-                      lineNum={i}
-                      lineProps={getLineProps}
-                      tokenProps={getTokenProps}
-                      lineNumPadLen={paddingLength}
-                      showIcon={true}
-                      annotationText={indexMap[i].annotation_text}
-                    />
-                  );
-                } else {
-                  return (
-                    <CodeLine
-                      line={line}
-                      lineNum={i}
-                      lineProps={getLineProps}
-                      tokenProps={getTokenProps}
-                      lineNumPadLen={paddingLength}
-                      showIcon={false}
-                      annotationText={""}
-                    />
-                  );
-                }
-              })}
-            </pre>
-          )}
-        </Highlight>
-        {/*<CommentContainer />*/}
-      </>
-    );
-  }
+  return (
+    <div id="block-code-display">
+      <h1>{dropTitle}</h1>
+      <h3>{"Language: ".concat(dropLanguage)}</h3>
+      <Highlight
+        Prism={Prism}
+        theme={theme}
+        code={dropText}
+        language={dropLanguage}
+      >
+        {({ className, style, tokens, getLineProps, getTokenProps }) => (
+          <pre className={className} style={style}>
+            {tokens.map((line, i) => {
+              if (i in indexMap) {
+                console.log(indexMap[i]);
+                return (
+                  <CodeLine
+                    line={line}
+                    lineNum={i}
+                    lineProps={getLineProps}
+                    tokenProps={getTokenProps}
+                    lineNumPadLen={paddingLength}
+                    showIcon={true}
+                    annotationText={indexMap[i].annotation_text}
+                  />
+                );
+              } else {
+                return (
+                  <CodeLine
+                    line={line}
+                    lineNum={i}
+                    lineProps={getLineProps}
+                    tokenProps={getTokenProps}
+                    lineNumPadLen={paddingLength}
+                    showIcon={false}
+                    annotationText={""}
+                  />
+                );
+              }
+            })}
+          </pre>
+        )}
+      </Highlight>
+    </div>
+  );
 }
