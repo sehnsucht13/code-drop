45423dd381b29273055ca20ab44504ae815fb699
Yavor Konstantinov <ykonstantinov1@gmail.com>
2021-01-30 17:21:45 -0800

    Implement container to display code blocks.

---
+++ frontend/src/components/CodeDisplayContainer.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import NavBar from "./Navbar";
+import { Tabs, Tab } from "react-bootstrap";
+
+function CodeDisplayContainer({ dropID }) {
+  const [isLoading, setIsLoading] = useState(true);
+  const [codeDrop, setCodeDrop] = useState({});
+  const [codeDropAnnotations, setcodeDropAnnotations] = useState([]);
+  const [currentTab, setcurrentTab] = useState("block");
+
+  const handleTabClick = (tabKey) => {
+    setcurrentTab(tabKey);
+  };
+
+  // Load the data from the server
+  useEffect(() => {
+    setIsLoading(true);
+    axios
+      .get(`/drops/21`)
+      .then((response) => {
+        console.log("Response from the server is", response.data);
+        setCodeDrop(response.data.codeDrop);
+        setcodeDropAnnotations(response.data.dropAnnotations);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.log("Error encountered", err);
+        setIsLoading(false);
+      });
+  }, []);
+
+  return (
+    <div>
+      <NavBar />
+      {isLoading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <Tabs activeKey={currentTab} onSelect={handleTabClick}>
+          <Tab eventKey="block" title="Block View"></Tab>
+          <Tab eventKey="inline" title="Inline View"></Tab>
+        </Tabs>
+      )}
+    </div>
+  );
+}
+
+export default CodeDisplayContainer;
