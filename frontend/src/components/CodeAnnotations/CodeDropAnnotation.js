fe8f88e5bc82506f0b4f3f1b0e4d064b7ca5156c
Yavor Konstantinov <ykonstantinov1@gmail.com>
2021-02-01 14:00:05 -0800

    Implement basic validation of annotation input fields.

--- frontend/src/components/CodeAnnotations/CodeDropAnnotation.js
+++ frontend/src/components/CodeAnnotations/CodeDropAnnotation.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Form, Col, Button, ButtonGroup } from "react-bootstrap";
+import { Form, Row, FormControl, Col, Button } from "react-bootstrap";
 import { connect } from "react-redux";
 import CommentEditor from "../CommentEditor/CommentEditor";
 import {
@@ -15,47 +15,78 @@ function CodeDropAnnotation({
   startIdx,
   endIdx,
   isEdited,
+  editorInstance,
   deleteAnnotation,
-  setEditStatus,
   saveAnnotationState,
 }) {
   const [startLineNum, setStartLineNum] = useState(startIdx);
   const [endLineNum, setendLineNum] = useState(endIdx);
   const [commentText, setCommentText] = useState(text);
 
-  const handleStartLineNumChange = (ev) => {
-    if (!isEdited) {
-      setEditStatus(true, index);
+  const [startLineWarningMsg, setStartLineWarningMsg] = useState(undefined);
+  const [endLineWarningMsg, setEndLineWarningMsg] = useState(undefined);
+
+  const validateStartIndex = (val) => {
+    if (isNaN(val)) {
+      setStartLineWarningMsg("Start index must be an integer!");
+    } else if (val > editorInstance.lineCount()) {
+      // Case of input referencing a line number that does not exist
+      setStartLineWarningMsg(
+        "Start index cannot exceed the total number of lines of code!"
+      );
+    } else if (val > endLineNum) {
+      setStartLineWarningMsg("Start Index cannot exceed the End Index.");
+    } else {
+      setStartLineWarningMsg(undefined);
     }
+  };
+
+  const validateEndIndex = (val) => {
+    if (isNaN(val)) {
+      setEndLineWarningMsg("End index must be an integer!");
+    } else if (val > editorInstance.lineCount()) {
+      // Case of input referencing a line number that does not exist
+      setEndLineWarningMsg(
+        "End index cannot exceed the total number of lines of code!"
+      );
+    } else if (val < startLineNum) {
+      setEndLineWarningMsg("End Index cannot be smaller than the Start Index");
+    } else {
+      setEndLineWarningMsg(undefined);
+    }
+  };
+
+  const handleStartLineNumChange = (ev) => {
+    console.log("Number of lines in editor are:", editorInstance.lineCount());
+    validateStartIndex(parseInt(ev.target.value));
     setStartLineNum(ev.target.value);
   };
+
   const handleEndLineNumChange = (ev) => {
-    if (!isEdited) {
-      setEditStatus(true, index);
-    }
+    validateEndIndex(parseInt(ev.target.value));
     setendLineNum(ev.target.value);
   };
 
+  const handleStartLineBlur = () => {
+    validateStartIndex(parseInt(startLineNum));
+    saveAnnotationState(startLineNum, endLineNum, commentText, index);
+  };
+
+  const handleEndLineBlur = () => {
+    validateEndIndex(parseInt(endLineNum));
+    saveAnnotationState(startLineNum, endLineNum, commentText, index);
+  };
+
   const handleTextChange = (newText) => {
-    if (!isEdited) {
-      setEditStatus(true, index);
-    }
     setCommentText(newText);
+  };
+
+  const handleTextBlur = () => {
+    saveAnnotationState(startLineNum, endLineNum, commentText, index);
   };
 
   const handleDelete = () => {
     deleteAnnotation(index);
-  };
-
-  const handleSave = () => {
-    saveAnnotationState(startLineNum, endLineNum, commentText, index);
-  };
-
-  const handleDiscard = () => {
-    setCommentText(text);
-    setendLineNum(endIdx);
-    setStartLineNum(startIdx);
-    setEditStatus(false, index);
   };
 
   return (
@@ -67,23 +98,46 @@ function CodeDropAnnotation({
             <Form.Control
               value={startLineNum}
               onChange={handleStartLineNumChange}
+              onBlur={handleStartLineBlur}
+              type="number"
+              as="input"
+              min="1"
+              isInvalid={startLineWarningMsg ? true : false}
             ></Form.Control>
+            <FormControl.Feedback type="invalid">
+              {startLineWarningMsg}
+            </FormControl.Feedback>
           </Form.Group>
           <Form.Group as={Col}>
             <Form.Label>End Line Number</Form.Label>
             <Form.Control
               value={endLineNum}
               onChange={handleEndLineNumChange}
+              onBlur={handleEndLineBlur}
+              type="number"
+              as="input"
+              min="1"
+              isInvalid={endLineWarningMsg ? true : false}
             ></Form.Control>
+            <FormControl.Feedback type="invalid">
+              {endLineWarningMsg}
+            </FormControl.Feedback>
           </Form.Group>
         </Form.Row>
       </Form>
-      <CommentEditor text={commentText} setText={handleTextChange} />
-      <ButtonGroup>
-        <Button onClick={handleSave}>Save</Button>
-        <Button onClick={handleDelete}>Delete</Button>
-        {isEdited && <Button onClick={handleDiscard}>Discard Changes</Button>}
-      </ButtonGroup>
+      <CommentEditor
+        value={commentText}
+        onChange={handleTextChange}
+        onBlur={handleTextBlur}
+      />
+      <Row className="justify-content-end">
+        <Button
+          onClick={handleDelete}
+          style={{ marginRight: "15px", marginTop: "5px" }}
+        >
+          Delete Annotation
+        </Button>
+      </Row>
     </div>
   );
 }
@@ -97,6 +151,7 @@ const mapStateToProps = (state, ownProps) => {
     startIdx: annotationInfo.start,
     endIdx: annotationInfo.end,
     isEdited: annotationInfo.isEdited,
+    editorInstance: state.editor.instance,
   };
 };
 
