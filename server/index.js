a92c73009806c39f4d24f255838c3d67714a7ed2
Yavor Konstantinov <ykonstantinov1@gmail.com>
2021-02-04 16:32:25 -0800

    Implement new routes. Refactor existing routes to check login status.
    - Add star/comment put and get routes.

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ const bcrypt = require("bcryptjs");
 const session = require("express-session");
 
 const db = require("./models");
+const { Users, Drops } = require("./models");
 
 let app = express();
 
@@ -33,6 +34,7 @@ require("./passportConfig")(passport);
 app.post("/register", async (req, res) => {
   let password = req.body.password;
   let username = req.body.username;
+  console.log("password and username for register", password, username);
   if (!password || !username) {
     res.json({ msg: "Password or Username are not provided" }).status(401);
   }
@@ -42,63 +44,115 @@ app.post("/register", async (req, res) => {
     },
   });
   if (user !== null) {
-    res.json({ msg: "This username is already taken!" });
+    res
+      .json({ status: "TAKEN", msg: "This username is already taken!" })
+      .status(200);
     return;
   }
   const hashedPassword = await bcrypt.hash(password, 10);
-  let newUser = db.Users.create({
+  let newUser = await db.Users.create({
     username: username,
     password: hashedPassword,
   });
   console.log("The new user created is", newUser);
-  res.json({ msg: "User created successfully!" }).status(200);
+  res.json({ status: "OK", msg: "User created successfully!" }).status(200);
 });
 
 app.post("/login", passport.authenticate("local"), (req, res) => {
   console.log("Successful auth with", req.body);
-  res.redirect("/");
-  // console.log("Received a login request with", req.body);
-  // passport.authenticate("local", (err, user, info) => {
-  //   if (err) throw err;
-  //   if (!user) {
-  //     console.log("User does not exist");
-  //     res.status(200).json({ msg: "User does not exist" });
-  //   } else {
-  //     req.logIn(user, (err) => {
-  //       if (err) {
-  //         console.log("Error when loggin in is", err);
-  //         res.status(401).end();
-  //       } else {
-  //         console.log("User logged in successfully", req.user);
-  //         res.redirect("/");
-  //         //res.status(200).end()
-  //       }
-  //     });
-  //   }
-  // })(req, res, next);
+  res.status(200).end();
 });
 
-app.get("/user", function (req, res) {
+app.get("/auth", function (req, res) {
   console.log("Got the user route", req.user);
   res.send(req.user);
 });
 
 app.get("/logout", function (req, res) {
   console.log("Got the logout route", req.user);
-  req.logout();
+  if (req.user !== undefined) {
+    req.logout();
+    console.log("User logged out");
+  }
+  res.status(200).end();
 });
 
 app.get("/", function (req, res) {
   res.json("Hello World from the server!");
 });
 
-app.post("/drops", async (req, res) => {
+app.post("/drop/:dropId/comments", async (req, res) => {
+  // User is not logged in
+  if (req.user === undefined) {
+    res.status(401).end();
+    return;
+  }
+  const commentBody = req.body.text || "";
+  const dropId = req.params.dropId;
+  if (commentBody.length === 0) {
+    res.status(400).end();
+    return;
+  }
+  let dropCount = await db.Drops.count({ where: { id: dropId } });
+  if (dropCount === 0) {
+    res.status(404).end();
+    return;
+  }
+  // console.log("Counted", userCount);
+
+  let commentModel = await db.Comments.create({
+    text: commentBody,
+    dropId: dropId,
+    userId: req.user.uid,
+  });
+  if (commentModel === null) {
+    res.status(500).end();
+    return;
+  }
+
+  res.json({ id: commentModel.dataValues.id }).status(200);
+});
+
+app.post("/drop/:dropId/stars", async (req, res) => {
+  // User is not logged in
+  if (req.user === undefined) {
+    res.status(401).end();
+    return;
+  }
+  const dropId = req.params.dropId;
+
+  let dropCount = await db.Drops.count({ where: { id: dropId } });
+  if (dropCount === 0) {
+    res.status(404).end();
+    return;
+  }
+
+  let starModel = await db.Stars.create({
+    dropId: dropId,
+    userId: req.user.uid,
+  });
+  if (commentModel === null) {
+    res.status(500).end();
+    return;
+  }
+
+  res.status(200).end();
+});
+
+app.post("/drop", async (req, res) => {
   let title = req.body.title;
   let lang = req.body.lang;
   let visibility = req.body.visibility;
   let textBody = req.body.text;
-  let description = req.body.description;
-  let annotations = req.body.annotations;
+  let description = req.body.description || "";
+  let annotations = req.body.annotations || [];
+
+  // User is not logged in
+  if (req.user === undefined) {
+    res.status(401).end();
+    return;
+  }
+
   console.log(
     "SERVER RECEIVED",
     title,
@@ -115,25 +169,160 @@ app.post("/drops", async (req, res) => {
     visibility: visibility,
     text: textBody,
     description: description,
+    userId: req.user.uid,
   });
-
-  let newDropId = dropRecordInfo.dataValues.id;
-  console.log(newDropId);
+  // Todo: Handle insertion error
 
   annotations.forEach((annotation) => {
     db.DropAnnotations.create({
       startLine: annotation.start,
       endLine: annotation.end,
       annotation_text: annotation.text,
-      dropId: newDropId,
+      dropId: dropRecordInfo.dataValues.id,
+      userId: req.user.uid,
     });
+    // Todo: Handle insertion error
   });
-  res.json({ id: newDropId }).status(200);
+  res.json({ id: dropRecordInfo.dataValues.id }).status(200);
 });
 
-app.delete("/drops/:dropId", (req, res) => {
+app.delete("/drop/:dropId/comments/:cId", async (req, res) => {
   let dropId = req.params.dropId;
+  let commentId = req.params.cId;
+
+  if (user === undefined) {
+    res.json({ msg: "Unauthorized" }).status(401);
+    return;
+  }
+  let commentInstance = await db.Comments.findOne({
+    where: { userId: user.uid, dropId: dropId, id: commentId },
+  });
+  // If the drop does not exist
+  if (commentInstance === null) {
+    res.status(404).end();
+    return;
+  }
+
+  commentInstance.destroy();
+  res.status(200).end();
 });
+
+app.delete("/drop/:dropId/stars", async (req, res) => {
+  let dropId = req.params.dropId;
+
+  if (user === undefined) {
+    res.json({ msg: "Unauthorized" }).status(401);
+    return;
+  }
+  let starInstance = await db.Stars.findOne({
+    where: { userId: user.uid, dropId: dropId },
+  });
+
+  // If the drop does not exist
+  if (starInstance === null) {
+    res.status(404).end();
+    return;
+  }
+
+  starInstance.destroy();
+  res.status(200).end();
+});
+
+app.delete("/drop/:dropId", async (req, res) => {
+  let dropId = req.params.dropId;
+  let user = req.user;
+  console.log("ID and user are", dropId, user);
+
+  if (user === undefined) {
+    res.json({ msg: "Unauthorized" }).status(401);
+  } else {
+    let drop = await db.Drops.findOne({
+      where: { userId: user.uid, id: dropId },
+    });
+    // If the drop does not exist
+    if (drop === null) {
+      res.status(404).end();
+      return;
+    }
+
+    drop.destroy();
+    res.status(200).end();
+  }
+});
+
+app.delete("/users/:userId", async (req, res) => {
+  const userId = req.params.userId;
+  if (req.user === undefined || req.user.uid !== userId) {
+    res.status(401).end();
+    return;
+  }
+
+  let userModel = db.Users.findByPk(userId);
+  if (userModel === null) {
+    res.status(200).end();
+  } else {
+    userModel.destroy();
+    res.status(200).end();
+  }
+});
+
+app.get("/users/:userId/stars", async (req, res) => {
+  const userId = req.params.userId;
+  if (req.user === undefined || req.user.uid !== userId) {
+    res.status(404).end();
+    return;
+  }
+
+  let starModel = db.Stars.findAll({ where: { userId: userId } });
+  if (starModel === null) {
+    res.json([]).status(200);
+  } else {
+    const starredDrops = starModel.map(async (star) => {
+      const dropsModel = db.Drops.findOne({
+        where: { id: star.dataValues.dropId },
+        attributes: ["id", "title", "description", "lang"],
+      });
+      return dropsModel.dataValues;
+    });
+    res.json(starredDrops).status(200);
+  }
+});
+
+app.get("/users/:userId", async (req, res) => {
+  const userId = req.params.userId;
+  if (req.user === undefined || req.user.uid !== userId) {
+    res.status(401).end();
+    return;
+  }
+
+  let userModel = db.Users.findByPk(userId, {
+    attributes: ["id", "text", "description", "numStars", "numForks"],
+  });
+  if (userModel === null) {
+    res.status(404).end();
+  } else {
+    res.json(userModel.dataValues).status(200);
+  }
+});
+
+app.get("/drops/:dropId/comments", async (req, res) => {
+  const dropId = req.params.dropId;
+  const comments = await db.Comments.findAll({
+    where: { dropId: dropId },
+    attributes: ["id", "text", "updatedAt", "userId"],
+  });
+  if (comments !== null) {
+    const filteredComments = comments.map(async (comment) => {
+      const commentAuthor = await db.Users.findByPk(comment.dataValues.userId, {
+        attributes: ["username"],
+      });
+      return { ...comment, username: commentAuthor.dataValues.username };
+    });
+    res.json(comments).status(200);
+  }
+  res.json({}).status(200);
+});
+
 app.put("/drops/:dropId", (req, res) => {
   let dropId = req.params.dropId;
 });
@@ -158,36 +347,36 @@ app.get("/drops/paginate", async (req, res) => {
   }
 });
 
-app.get("/drops/search", async (req, res) => {
-  console.log(req.query);
-  res.json({ status: "found" }).status(200);
-});
-
-app.get("/drops/:dropId", async (req, res) => {
+app.get("/drop/:dropId", async (req, res) => {
   //res.json("Hello from drops get");
   let dropId = req.params.dropId;
   let dropRecord = await db.Drops.findByPk(dropId, {
     attributes: ["id", "title", "lang", "visibility", "text", "description"],
   });
-  console.log("Retrieve a record", dropRecord);
-  let annotations = await db.DropAnnotations.findAll({
-    attributes: {
-      exclude: ["createdAt", "updatedAt", "dropId"],
-    },
-    where: {
-      dropId: dropRecord.dataValues.id,
-    },
-  });
-  console.log(
-    "Annotations retrieve are",
-    annotations.map((annotation) => annotation.dataValues)
-  );
 
-  const respObject = {
-    codeDrop: dropRecord.dataValues,
-    dropAnnotations: annotations.map((annotation) => annotation.dataValues),
-  };
-  res.json(respObject).status(200);
+  console.log("Retrieve a record", dropRecord);
+  if (dropRecord === null) {
+    res.status(400).end();
+  } else {
+    let annotations = await db.DropAnnotations.findAll({
+      attributes: {
+        exclude: ["createdAt", "updatedAt", "dropId"],
+      },
+      where: {
+        dropId: dropRecord.dataValues.id,
+      },
+    });
+    console.log(
+      "Annotations retrieve are",
+      annotations.map((annotation) => annotation.dataValues)
+    );
+
+    const respObject = {
+      codeDrop: dropRecord.dataValues,
+      dropAnnotations: annotations.map((annotation) => annotation.dataValues),
+    };
+    res.json(respObject).status(200);
+  }
 });
 
 db.sequelize
