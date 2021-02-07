6fd5ecb0ae0ab9757fad91dc28d5a2a7317bfb5b
Yavor Konstantinov <ykonstantinov1@gmail.com>
2021-02-07 11:04:59 -0800

    Implement tests for profile.

---
+++ server/__tests__/profile/profileDeleteTests.js
@@ -0,0 +1,200 @@
+const { db, app } = require("../../expressSetup");
+const supertest = require("supertest");
+
+describe("Profile Delete Tests", () => {
+  let sessionId_1;
+  let sessionId_2;
+  let dropIds = [];
+  beforeAll(async (done) => {
+    const dbStatus = await db.sequelize.sync();
+    // Create user one
+    const agent = supertest.agent(app);
+
+    const registerResp = await agent.post("/auth/register/").send({
+      username: "tdp_1",
+      password: "tdp_1",
+    });
+    expect(registerResp.statusCode).toBe(200);
+
+    const loginResp = await agent.post("/auth/login/").send({
+      username: "tdp_1",
+      password: "tdp_1",
+    });
+    expect(loginResp.statusCode).toBe(200);
+
+    const sessionResp = await agent.get("/auth/session/");
+    expect(sessionResp.statusCode).toBe(200);
+    sessionId_1 = sessionResp.body;
+
+    const logout = await agent.get("/auth/logout/");
+    expect(logout.statusCode).toBe(200);
+
+    // Create user two
+    const agent_2 = supertest.agent(app);
+
+    const registerResp_2 = await agent_2.post("/auth/register/").send({
+      username: "tdp_2",
+      password: "tdp_2",
+    });
+    expect(registerResp_2.statusCode).toBe(200);
+
+    const loginResp_2 = await agent_2.post("/auth/login/").send({
+      username: "tdp_2",
+      password: "tdp_2",
+    });
+    expect(loginResp_2.statusCode).toBe(200);
+
+    const sessionResp_2 = await agent_2.get("/auth/session/");
+    expect(sessionResp_2.statusCode).toBe(200);
+    sessionId_2 = sessionResp_2.body;
+
+    const logout_2 = await agent_2.get("/auth/logout/");
+    expect(logout_2.statusCode).toBe(200);
+
+    // Create drops for user one
+    const drop1 = await db.Drops.create({
+      title: "public drop for delete user",
+      lang: "python",
+      visibility: true,
+      text: "# Hello world",
+      description: "Test Description",
+      userId: sessionResp.body.uid,
+    });
+    dropIds.push(drop1.dataValues.id);
+
+    const drop2 = await db.Drops.create({
+      title: "hidden drop for delete user",
+      lang: "python",
+      visibility: false,
+      text: "# Hello world 2",
+      description: "Test Description 2",
+      userId: sessionResp.body.uid,
+    });
+    dropIds.push(drop2.dataValues.id);
+
+    const drop3 = await db.Drops.create({
+      title: "public drop for delete user 2",
+      lang: "c++",
+      visibility: true,
+      text: "# Hello world 3",
+      description: "Test Description 3",
+      userId: sessionResp.body.uid,
+    });
+    dropIds.push(drop3.dataValues.id);
+
+    done();
+  });
+
+  afterAll(async (done) => {
+    dropIds.forEach(async (dropId) => {
+      const dropModel = await db.Drops.findOne({ where: { id: dropId } });
+      if (dropModel !== null) {
+        await dropModel.destroy();
+      }
+    });
+
+    const userModel_1 = await db.Users.findOne({
+      where: { username: "tdp_1" },
+    });
+
+    if (userModel_1 !== null) {
+      await userModel_1.destroy();
+    }
+    const userModel_2 = await db.Users.findOne({
+      where: { username: "tdp_2" },
+    });
+
+    if (userModel_2 !== null) {
+      await userModel_2.destroy();
+    }
+
+    await db.sequelize.close();
+    done();
+  });
+
+  it("should not delete profile of a user while not logged in", async (done) => {
+    const agent = supertest.agent(app);
+    const deleteResp = await agent.delete(`/user/${sessionId_1.uid}`);
+    expect(deleteResp.statusCode).toBe(401);
+
+    done();
+  });
+  it("should not delete profile of another user while logged in", async (done) => {
+    const agent = supertest.agent(app);
+    const agent_2 = supertest.agent(app);
+
+    const loginResp_2 = await agent_2.post("/auth/login/").send({
+      username: "tdp_2",
+      password: "tdp_2",
+    });
+    expect(loginResp_2.statusCode).toBe(200);
+
+    const sessionResp_2 = await agent_2.get("/auth/session/");
+    expect(sessionResp_2.statusCode).toBe(200);
+    expect(sessionResp_2.body).toEqual(sessionId_2);
+
+    const deleteResp = await agent.delete(`/user/${sessionId_1.uid}`);
+    expect(deleteResp.statusCode).toBe(401);
+    expect(deleteResp.body).toEqual({});
+
+    const logout_2 = await agent_2.get("/auth/logout/");
+    expect(logout_2.statusCode).toBe(200);
+
+    done();
+  });
+  it("should delete profile of logged in user without drops and log them out", async (done) => {
+    const agent_2 = supertest.agent(app);
+
+    const loginResp_2 = await agent_2.post("/auth/login/").send({
+      username: "tdp_2",
+      password: "tdp_2",
+    });
+    expect(loginResp_2.statusCode).toBe(200);
+
+    const sessionResp_1 = await agent_2.get("/auth/session/");
+    expect(sessionResp_1.statusCode).toBe(200);
+    expect(sessionResp_1.body).toEqual(sessionId_2);
+
+    const deleteResp = await agent_2.delete(`/user/${sessionResp_1.body.uid}`);
+    expect(deleteResp.statusCode).toBe(200);
+    expect(deleteResp.body).toEqual({});
+
+    const userModel = await db.Users.findOne({
+      where: { id: sessionResp_1.body.uid },
+    });
+    expect(userModel).toBe(null);
+
+    const sessionResp_2 = await agent_2.get("/auth/session/");
+    expect(sessionResp_2.statusCode).toBe(200);
+    expect(sessionResp_2.body).toEqual({});
+    done();
+  });
+  // TODO: Finish this test!
+  // it("should delete profile of logged in user with drops and log them out", async (done) => {
+  // const agent_2 = supertest.agent(app);
+
+  // const loginResp_2 = await agent_2.post("/auth/login/").send({
+  //   username: "tdp_1",
+  //   password: "tdp_1",
+  // });
+  // expect(loginResp_2.statusCode).toBe(200);
+
+  // const sessionResp_1 = await agent_2.get("/auth/session/");
+  // expect(sessionResp_1.statusCode).toBe(200);
+  // expect(sessionResp_1.body).toEqual(sessionId_2);
+
+  // const deleteResp = await agent_2.delete(`/user/${sessionResp_1.body.uid}`);
+  // expect(deleteResp.statusCode).toBe(200);
+  // expect(deleteResp.body).toEqual({});
+
+  // const userModel = await db.Users.findOne({
+  //   where: { id: sessionResp_1.body.uid },
+  // });
+  // expect(userModel).toBe(null);
+
+  // const sessionResp_2 = await agent_2.get("/auth/session/");
+  // expect(sessionResp_2.statusCode).toBe(200);
+  // expect(sessionResp_2.body).toEqual({});
+  // done();
+  // });
+});
---
+++ server/__tests__/profile/profileRetrieveTests.js
@@ -0,0 +1,167 @@
+const { db, app } = require("../../expressSetup");
+const supertest = require("supertest");
+
+describe("Profile Tests", () => {
+  let sessionId;
+  let dropIds = [];
+  beforeAll(async (done) => {
+    const dbStatus = await db.sequelize.sync();
+    const agent = supertest.agent(app);
+
+    const registerResp = await agent
+      .post("/auth/register/")
+      .send({ username: "test_profile", password: "test_profile" });
+    expect(registerResp.statusCode).toBe(200);
+
+    const loginResp = await agent
+      .post("/auth/login/")
+      .send({ username: "test_profile", password: "test_profile" });
+    expect(loginResp.statusCode).toBe(200);
+
+    const sessionResp = await agent.get("/auth/session/");
+    expect(sessionResp.statusCode).toBe(200);
+    sessionId = sessionResp.body;
+
+    const logout = await agent.get("/auth/logout/");
+    expect(logout.statusCode).toBe(200);
+
+    const drop1 = await db.Drops.create({
+      title: "public drop 1",
+      lang: "python",
+      visibility: true,
+      text: "# Hello world",
+      description: "Test Description",
+      userId: sessionResp.body.uid,
+    });
+    dropIds.push(drop1.dataValues.id);
+
+    const drop2 = await db.Drops.create({
+      title: "hidden drop",
+      lang: "python",
+      visibility: false,
+      text: "# Hello world 2",
+      description: "Test Description 2",
+      userId: sessionResp.body.uid,
+    });
+    dropIds.push(drop2.dataValues.id);
+
+    const drop3 = await db.Drops.create({
+      title: "public drop 2",
+      lang: "c++",
+      visibility: true,
+      text: "# Hello world 3",
+      description: "Test Description 3",
+      userId: sessionResp.body.uid,
+    });
+    dropIds.push(drop3.dataValues.id);
+
+    done();
+  });
+
+  afterAll(async (done) => {
+    dropIds.forEach(async (dropId) => {
+      const dropModel = await db.Drops.findOne({ where: { id: dropId } });
+      if (dropModel !== null) {
+        await dropModel.destroy();
+      }
+    });
+
+    const userModel = await db.Users.findOne({
+      where: { username: "test_profile" },
+    });
+
+    if (userModel !== null) {
+      await userModel.destroy();
+    }
+
+    await db.sequelize.close();
+    done();
+  });
+
+  it("Find current user's profile while authenticated", async () => {
+    let agent = supertest.agent(app);
+
+    const loginResp = await agent
+      .post("/auth/login/")
+      .send({ username: "test_profile", password: "test_profile" });
+    expect(loginResp.statusCode).toBe(200);
+
+    const sessionResp = await agent.get("/auth/session/");
+    expect(sessionResp.statusCode).toBe(200);
+    expect(sessionResp.body).not.toEqual({});
+    console.log("SERVER SESSION", sessionResp.body);
+
+    const userResp = await agent.get(`/user/${sessionId.uid}/profile`);
+    expect(userResp.statusCode).toBe(200);
+    console.log("Server response!", userResp.body);
+
+    expect(userResp.body).toEqual({
+      profile: {
+        id: sessionId.uid,
+        username: "test_profile",
+        description: "",
+        numStars: 0,
+        numForks: 0,
+      },
+      drops: [
+        { id: dropIds[0], title: "public drop 1", lang: "python" },
+        { id: dropIds[1], title: "hidden drop", lang: "python" },
+        { id: dropIds[2], title: "public drop 2", lang: "c++" },
+      ],
+    });
+
+    const logout = await agent.get("/auth/logout/");
+    expect(logout.statusCode).toBe(200);
+  });
+
+  //   it("Find another user's profile while authenticated", async () => {
+  //     let userResp = await agent.get("/user/523/profile");
+  //     expect(userResp.statusCode).toBe(200);
+  //   });
+
+  it("Find existing user's profile while not authenticated", async () => {
+    let agent = supertest.agent(app);
+    let userResp = await agent.get(`/user/${sessionId.uid}/profile`);
+    expect(userResp.statusCode).toBe(200);
+    // console.log(userResp.body);
+    expect(userResp.body).toEqual({
+      profile: {
+        id: sessionId.uid,
+        username: "test_profile",
+        description: "",
+        numStars: 0,
+        numForks: 0,
+      },
+      drops: [
+        { id: dropIds[0], title: "public drop 1", lang: "python" },
+        { id: dropIds[2], title: "public drop 2", lang: "c++" },
+      ],
+    });
+  });
+
+  it("Find nonexistant profile while not authenticated", async (done) => {
+    let agent = supertest.agent(app);
+
+    const loginResp = await agent
+      .post("/auth/login/")
+      .send({ username: "test_profile", password: "test_profile" });
+    expect(loginResp.statusCode).toBe(200);
+
+    let userProfileResp = await agent.get("/user/523/profile");
+    expect(userProfileResp.statusCode).toBe(404);
+
+    const logout = await agent.get("/auth/logout/");
+    expect(logout.statusCode).toBe(200);
+
+    done();
+  });
+
+  it("Find nonexistant profile while authenticated", async (done) => {
+    let agent = supertest.agent(app);
+
+    let userProfileResp = await agent.get("/user/523/profile");
+    expect(userProfileResp.statusCode).toBe(404);
+
+    done();
+  });
+});
