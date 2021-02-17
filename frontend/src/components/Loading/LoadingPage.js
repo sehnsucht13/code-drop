0eeb213f0837df38f746f19e8ca05710b2ece495
Yavor Konstantinov <ykonstantinov1@gmail.com>
2021-02-16 23:07:06 -0800

    Fix alignment of loading page.

--- frontend/src/components/Loading/LoadingPage.js
+++ frontend/src/components/Loading/LoadingPage.js
@@ -1,12 +1,19 @@
 import React from "react";
-import { Spinner, Container } from "react-bootstrap";
+import { Spinner, Container, Row } from "react-bootstrap";
 
 function LoadingPage() {
   return (
-    <div className="d-flex flex-column align-items-center h-100">
-      <Spinner animation="grow"></Spinner>
-      <span className="display-4">Loading...</span>
-    </div>
+    <Container fluid style={{ height: "100%" }}>
+      <Row
+        className="d-flex flex-column align-items-center justify-content-center"
+        style={{ height: "100%" }}
+      >
+        <Spinner animation="grow"></Spinner>
+        <p className="display-4" style={{ paddingTop: "0.2em" }}>
+          Loading...
+        </p>
+      </Row>
+    </Container>
   );
 }
 
