b959c0ad08992c93689c544615797b9b8a0d95ee
Yavor Konstantinov <ykonstantinov1@gmail.com>
2021-02-19 14:24:29 -0800

    Refactor comment display to use Cards.

--- frontend/src/components/CommentDisplay/CommentDisplay.js
+++ frontend/src/components/CommentDisplay/CommentDisplay.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
-import { Row, Button, Dropdown, Alert } from "react-bootstrap";
+import { Row, Button, Dropdown, Alert, Card, Col } from "react-bootstrap";
 import { BsThreeDotsVertical } from "react-icons/bs";
 import ReactMarkdown from "react-markdown";
 import axios from "axios";
@@ -97,7 +97,10 @@ function CommentDisplay({
           onChange={handleEditorOnChange}
           onBlur={handleEditorOnBlur}
         />
-        <Row className="justify-content-end" style={{ paddingRight: "1rem" }}>
+        <Row
+          className="justify-content-end"
+          style={{ paddingRight: "1rem", paddingTop: "0.5rem" }}
+        >
           <Button onClick={handleSaveClick}>Save</Button>
           <Button onClick={handleDiscardClick}>Discard</Button>
         </Row>
@@ -107,41 +110,67 @@ function CommentDisplay({
     return (
       <div
         key={commentId}
-        style={{ paddingLeft: "1rem", paddingRight: "1rem" }}
+        style={{
+          paddingLeft: "1rem",
+          paddingRight: "1rem",
+          paddingTop: "0.5rem",
+        }}
       >
         {hasIssue && (
           <Alert variant="danger">
             There was an issue with deleting your comment. Please try again!
           </Alert>
         )}
-        <Row
-          className="justify-content-between"
-          style={{ backgroundColor: "lightgray" }}
-        >
-          <span
-            className="font-weight-bold"
-            style={{ fontSize: "1.5rem", paddingLeft: "0.5rem" }}
+        <Card>
+          <Card.Header
+            className="d-flex flex-row justify-content-between"
+            style={{
+              paddingTop: "0",
+              paddingBottom: "0.1rem",
+              paddingRight: "0",
+              paddingLeft: "0",
+            }}
           >
-            {authorUsername}
-          </span>
-          {isAuth && currentUser.uid === authorId && (
-            <Dropdown drop="down" style={{ paddingRight: "0.5rem" }}>
-              <Dropdown.Toggle>
-                <BsThreeDotsVertical />
-              </Dropdown.Toggle>
-              <Dropdown.Menu>
-                <Dropdown.Item onClick={handleEditClick}>Edit</Dropdown.Item>
-                <Dropdown.Item onClick={handleDeleteClick}>
-                  Delete
-                </Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
-          )}
-        </Row>
-        <ReactMarkdown>{commentText}</ReactMarkdown>
-        <Row>
-          <p>{"Last Update: ".concat(formatDate(lastUpdate))}</p>
-        </Row>
+            <Col xs={11}>
+              <p
+                style={{
+                  fontSize: "1.3rem",
+                  fontWeight: "500",
+                  paddingBottom: "0",
+                  marginBottom: "0",
+                }}
+              >
+                {authorUsername}
+              </p>
+
+              <p className="text-muted" style={{ marginBottom: "0" }}>
+                {formatDate(lastUpdate)}
+              </p>
+            </Col>
+            {isAuth && currentUser.uid === authorId && (
+              <Col xs={1} className="d-flex flex-column justify-content-end">
+                <Dropdown drop="down" style={{ paddingRight: "0.5rem" }}>
+                  <Dropdown.Toggle>
+                    <BsThreeDotsVertical />
+                  </Dropdown.Toggle>
+                  <Dropdown.Menu>
+                    <Dropdown.Item onClick={handleEditClick}>
+                      Edit
+                    </Dropdown.Item>
+                    <Dropdown.Item onClick={handleDeleteClick}>
+                      Delete
+                    </Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+              </Col>
+            )}
+          </Card.Header>
+          <Card.Body>
+            <Card.Text>
+              <ReactMarkdown>{commentText}</ReactMarkdown>
+            </Card.Text>
+          </Card.Body>
+        </Card>
       </div>
     );
   }
