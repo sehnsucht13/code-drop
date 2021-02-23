76662442bfcd59e313f302694e6658b5100cff01
Yavor Konstantinov <ykonstantinov1@gmail.com>
2021-02-22 17:28:38 -0800

    Fix error in package.json file. Fix error in shell script.

--- heroku-env-setup.sh
+++ heroku-env-setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 touch .env
-echo DB_HOST=${DB_HOST}
-echo DB_USER=${DB_USER}
-echo DB_PASS=${DB_PASS}
-echo DB_DATABASE_PROD=${DB_DATABASE_PROD}
+echo DB_HOST=${DB_HOST} >> .env
+echo DB_USER=${DB_USER} >> .env
+echo DB_PASS=${DB_PASS} >> .env
+echo DB_DATABASE_PROD=${DB_DATABASE_PROD} >> .env
 echo "Done setup"
