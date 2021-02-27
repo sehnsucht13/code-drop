c7ccb29b9d0a381f5d2a56b9329c6d088fae0813
Yavor Konstantinov <ykonstantinov1@gmail.com>
2021-02-27 11:40:27 -0800

    Add heroku post-setup hook. Serve react file from express.

--- server/expressSetup.js
+++ server/expressSetup.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const passport = require("passport");
+const path = require("path");
 //const cookieParser = require("cookie-parser");
 const session = require("express-session");
 const db = require("./models");
@@ -34,27 +35,33 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const corsConfig = {
-		origin:["http://localhost:3000", "http://localhost:5000", "https://code-drop.netlify.app", 'http://192.168.1.75:5000'],
-		methods:['GET', 'POST', 'PUT', 'DELETE'],
-		credentials:true,
-}
+  origin: [
+    "http://localhost:3000",
+    "http://localhost:5000",
+    "https://code-drop.netlify.app",
+    "http://192.168.1.75:5000",
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+};
 app.use(cors(corsConfig));
 
-app.set('trust proxy', 1);
+app.set("trust proxy", 1);
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
-    proxy:true,
-   	cookie:{
-		secure: true, 
-		maxAge: 1000 * 60 * 60 * 48, 
-   	},
+    proxy: true,
+    cookie: {
+      secure: true,
+      maxAge: 1000 * 60 * 60 * 48,
+    },
   })
 );
 //app.use(cookieParser(process.env.SESSION_SECRET));
 app.use(passport.initialize());
 app.use(passport.session());
 require("./passportConfig")(passport);
+app.use(express.static(path.join(__dirname, "frontend/build")));
 
 const auth = require("./Routes/auth")(passport);
 const drop = require("./Routes/drop");
@@ -64,7 +71,8 @@ app.use("/drop", drop);
 app.use("/user", user);
 
 app.get("/", (req, res) => {
-  res.status(200).json({ message: "Hello world from my heroku instance!" });
+  // res.status(200).json({ message: "Hello world from my heroku instance!" });
+  res.status(200).sendFile(path.join(__dirname + "/frontend/build/index.html"));
 });
 
 function startServer() {
