f4187b6e08f19a1969f7b1dc830243bf013d5a5d
Yavor Konstantinov <ykonstantinov1@gmail.com>
2021-02-27 12:15:37 -0800

    Tweak heroku setup file.

--- heroku-env-setup.sh
+++ heroku-env-setup.sh
@@ -6,5 +6,6 @@ echo DB_PASS=${DB_PASS} >> .env
 echo DB_DATABASE_PROD=${DB_DATABASE_PROD} >> .env
 echo SESSION_SECRET=${SESSION_SECRET} >> .env
 echo SKIP_PREFLIGHT_CHECK=true >> .env
+cat .env
 
 echo "Done .env setup!"
