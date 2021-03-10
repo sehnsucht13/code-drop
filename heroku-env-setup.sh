bd066aecad156074cab98be7c038f595797c0cdd
Yavor Konstantinov <ykonstantinov1@gmail.com>
2021-03-09 19:55:31 -0800

    Update Heroku environment setup script.

--- heroku-env-setup.sh
+++ heroku-env-setup.sh
@@ -7,4 +7,9 @@ echo DB_DATABASE_PROD=${DB_DATABASE_PROD} >> .env
 echo SESSION_SECRET=${SESSION_SECRET} >> .env
 echo SKIP_PREFLIGHT_CHECK=true >> .env
 
+echo AWS_REGION=${AWS_REGION} >> .env
+echo AWS_KEY=${AWS_KEY} >> .env
+echo AWS_SECRET=${AWS_SECRET} >> .env
+echo AWS_BUCKET_NAME=${AWS_BUCKET_NAME} >> .env
+
 echo "Done .env setup!"
